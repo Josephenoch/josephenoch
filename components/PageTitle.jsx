e6ff706d24bb8698289522244b7877e5a7e4546c
josephenoch <enochjoseph01@gmail.com>
2022-05-04 11:26:55 +0100

    :lipstick: made the active pages have a different style

--- components/PageTitle.jsx
+++ components/PageTitle.jsx
@@ -1,21 +1,24 @@
 import Link from 'next/link'
+import {useRouter} from 'next/router'
+
 
 
 import React from 'react'
 
 
 export const PageTitle = ({pageTitle, userName, lastItem, notLink}) => {
+  const router= useRouter()
   const styles = {
       rightPadding:userName ? "pr-24" : "",
       leftPadding:userName? "pl-5":"",
       contactStyle:lastItem? "last:ml-auto border-r-0 border-l-2 border-l-lines" : " border-r-2 border-r-lines",
+      activeStyle: router.pathname===`/${pageTitle}`|| (router.pathname==="/" && notLink)? "border-b-accent text-white":"hover:border-b-accent  hover:transition-[border] ease-in-ou border-b-primary-lighter text-secondary"
   }
-  console.log(notLink&&notLink ===true )
   return (
   
     <>
-        <Link href={!notLink ? pageTitle : "/" }>
-            <a className={`border-b-primary-lighter hover:border-b-accent  hover:transition-[border] ease-in-out ${styles.rightPadding} ${styles.contactStyle} ${styles.leftPadding} hover:text-white text-secondary inline-block px-6 py-3  border-b-2  `}>
+        <Link href={!notLink ? `/${pageTitle}` : "/" }>
+            <a className={`border-b-2  ${styles.activeStyle} t ${styles.rightPadding} ${styles.contactStyle} ${styles.leftPadding} hover:text-white  inline-block px-6 py-3   `}>
                 <h1 className=" text-xs">{userName?pageTitle.toLowerCase():`_${pageTitle.toLowerCase()}`}</h1>
             </a>
         </Link>
