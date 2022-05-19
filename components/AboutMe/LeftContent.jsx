5fd478f8e76727a8d089e257eec4fd83e5de69d5
josephenoch <enochjoseph01@gmail.com>
2022-05-19 14:22:20 +0100

    :zap: update getlines to generator function and optimize with useMemo

--- components/AboutMe/LeftContent.jsx
+++ components/AboutMe/LeftContent.jsx
@@ -1,20 +1,21 @@
-import React,{useRef,useLayoutEffect, useEffect,useState } from 'react'
+import React,{useMemo } from 'react'
 import getLines from '../../Helpers/getLines'
 export const LeftContent = () => {
-  const [lines,setLines] = useState([])
-  const pRef = useRef()
-  const text ="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur dolores odio excepturi! Placeat autem deserunt eum perspiciatis velit ullam numquam esse vel, pariatur corrupti voluptatum voluptas distinctio! Ipsa, assumenda harum.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur dolores odio excepturi! Placeat autem deserunt eum perspiciatis velit ullam numquam esse vel, pariatur corrupti voluptatum voluptas distinctio! Ipsa, assumenda harum."
+  const text ="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur dolores odio excepturi! Placeat autem deserunt eum perspiciatis velit ullam numquam esse vel, pariatur corrupti voluptatum voluptas distinctio! Ipsa, assumenda harum.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur dolores odio excepturi! Placeat autem deserunt eum perspiciatis velit ullam numquam esse vel, pariatur corrupti voluptatum voluptas distinctio! Ipsa, assumenda harum.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur dolores odio excepturi! Placeat autem deserunt eum perspiciatis velit ullam numquam esse vel, pariatur corrupti voluptatum voluptas distinctio! Ipsa, assumenda harum.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur dolores odio excepturi! Placeat autem deserunt eum perspiciatis velit ullam numquam esse vel, pariatur corrupti voluptatum voluptas distinctio! Ipsa, assumenda harum.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur dolores odio excepturi! Placeat autem deserunt eum perspiciatis velit ullam numquam esse vel, pariatur corrupti voluptatum voluptas distinctio! Ipsa, assumenda harum.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur dolores odio excepturi! Placeat autem deserunt eum perspiciatis velit ullam numquam esse vel, pariatur corrupti voluptatum voluptas distinctio! Ipsa, assumenda harum."
   
+  const WIDTH = 59
 
-
-  useLayoutEffect(()=>{
-    if(pRef.current){
-        pRef.current.style.display="none"
+  const lines = useMemo(()=>{
+    const genobj = getLines(text, WIDTH)
+    const res = []
+    let result = genobj.next()
+    while(!result.done){
+      res.push(result.value)
+      result = genobj.next()
     }
+    return res
   },[])
-  useEffect(()=>{
-    setLines(getLines(text))
-  },[])
+   
   return (
     <>
         <div className="inline-flex px-3 items-center border-r-2 py-2 border-lines">
@@ -33,7 +34,7 @@ export const LeftContent = () => {
             <li className="list-decimal"><span className="ml-5">{"*/"}</span></li>
 
           </ol>
-          <p ref={pRef} className="w-fit">
+          <p className="w-fit lg:hidden">
             {text}
           </p>
         </div>
