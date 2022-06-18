0f3a483fb0885d0b238f13c4d33230f786acceb0
josephenoch <enochjoseph01@gmail.com>
2022-06-19 00:20:39 +0100

    :fire: remove go bac function from 404 page

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import Button from '../Components/GeneralComponents/Button'
 import Link from "next/link"
-import {useRouter} from 'next/router'
 const NotFound = () => {
-  const router = useRouter()
   return (
     <div className="flex text-secondary w-full h-full flex-col items-center justify-center">
       <div className=" w-2/5 flex flex-col items-center justify-center">
@@ -11,16 +9,12 @@ const NotFound = () => {
         <p className="text-xs mb-2">404</p>
         <p className="text-xs mb-2">Page not found.</p>
         <p className="text-center text-xs mb-4">You likely forgot to export your component from the file {"it's"} defined in, or you might have mixed up default and named imports</p>
-        <div className="space-x-2">
-          <Button>
-            <Link href="/">
-              <a className="text-white">Go Home</a>
-            </Link>
-          </Button>
-          <Button onClick={()=>router.back()}>
-              Go back
-          </Button>  
-        </div>
+        <Button>
+          <Link href="/">
+            <a className="text-white">Go Home</a>
+          </Link>
+        </Button>
+          
       </div>
       
     </div>
