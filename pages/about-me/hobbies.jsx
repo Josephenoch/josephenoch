9e38cacaf151a439f9834d69e36c842cc84e4ada
josephenoch <enochjoseph01@gmail.com>
2022-06-23 09:29:03 +0100

    :mag: started using getStaticProps in all components

--- pages/about-me/hobbies.jsx
+++ pages/about-me/hobbies.jsx
@@ -1,13 +1,45 @@
-import React from "react"
+import React,{useState} from "react"
 import AboutLayout from '../../Layouts/AboutMeLayout/AboutLayout'
-import LeftComponent from '../../Components/AboutMe/LeftComponent'
 import SpotifyProvider from '../../Contexts/SpotifyContext'
 import RightContent from '../../Components/AboutMe/Hobbies/RightContent'
-
-const Hobbies = () => {
-  return (      
-      <>
-      <LeftComponent pageTitle="hobbies"/>
+import PageNav from "../../Components/AboutMe/PageNav"
+import LeftContent from "../../Components/AboutMe/LeftContent"
+import { getDocs,collection } from "firebase/firestore"
+import { db } from "../../firebase-config"
+const Hobbies = ({files}) => {
+  const [openEditor, setOpenEditor] = useState(
+      {
+        active:0,
+        pages:[
+        ]
+      }
+    )
+  const handleChangeEditor = ((selected) =>{
+    setOpenEditor(prevState=>{
+      if(prevState.pages.some(page=>page.title===selected.title)){
+        const newActive = prevState.pages.findIndex(page=>page.title==selected.title)
+        return {...prevState,active:newActive}
+      }
+      return {active:prevState.pages.length,pages:[...prevState.pages,selected]}
+    })
+  })
+  
+  const closeEditor = (item=>{
+    setOpenEditor(prevState=>{
+      const oldArray = [...prevState.pages]
+      const newArray = oldArray.filter(arr=>arr.title!==item.title)
+      return {active:0, pages:[...newArray]}
+    })
+  })
+  return (
+    <>
+        <nav className="w-full lg:w-40 h-fit lg:h-full lg:border-r-2 border-r-lines">
+            <PageNav files={files} handleChangeEditor={handleChangeEditor} pageTitle="hobbies"/>
+        </nav>
+        <article className="w-full  border-t-lines lg:w-[41.4%] h-fit lg:h-full text-secondary text-xs ">
+            <LeftContent  openEditor={openEditor} handleChangeEditor={handleChangeEditor} closeEditor={closeEditor}/>
+        </article>
+   
         <div className="hidden lg:block w-5 h-full mt-8 px-1 border-t-2 border-t-lines">
               <div className="w-3 h-full mx-auto mt-1 border-t-[6px] border-t-secondary">
               </div>
@@ -26,4 +58,22 @@ const Hobbies = () => {
 
 Hobbies.Layout = AboutLayout
 
+export const getStaticProps= async()=>{
+  const docsRef = collection(db,"hobbies")
+    const docs = await getDocs(docsRef)
+    const files = docs.docs.map(data=>data.data())
+    files.sort(dat=>{
+      if(dat.folderName==="none"){
+        return -1
+      }
+      else{
+        return 1
+      }
+    })
+    return {
+      props:{
+        files
+      }
+    }
+}
 export default Hobbies
--- pages/about-me/index.jsx
+++ pages/about-me/index.jsx
@@ -1,12 +1,44 @@
 import React,{useState, useEffect} from 'react'
+import { db } from '../../firebase-config'
+import { getDocs, collection } from 'firebase/firestore'
 import AboutLayout from '../../Layouts/AboutMeLayout/AboutLayout'
 import CodeShowCaseSection from "../../Components/AboutMe/PersonalInfo/CodeShowCaseSection"
-import LeftComponent from '../../Components/AboutMe/LeftComponent'
-const ProfessionalInfo = () => {  
+import PageNav from '../../Components/AboutMe/PageNav'
+import LeftContent from '../../Components/AboutMe/LeftContent'
+const ProfessionalInfo = ({files}) => {  
+  console.log(files)
+  const [openEditor, setOpenEditor] = useState(
+      {
+        active:0,
+        pages:[
+        ]
+      }
+    )
+  const handleChangeEditor = ((selected) =>{
+    setOpenEditor(prevState=>{
+      if(prevState.pages.some(page=>page.title===selected.title)){
+        const newActive = prevState.pages.findIndex(page=>page.title==selected.title)
+        return {...prevState,active:newActive}
+      }
+      return {active:prevState.pages.length,pages:[...prevState.pages,selected]}
+    })
+  })
+  
+  const closeEditor = (item=>{
+    setOpenEditor(prevState=>{
+      const oldArray = [...prevState.pages]
+      const newArray = oldArray.filter(arr=>arr.title!==item.title)
+      return {active:0, pages:[...newArray]}
+    })
+  })
   return (
     <>
-      
-      <LeftComponent pageTitle="professional-info"/>
+        <nav className="w-full lg:w-40 h-fit lg:h-full lg:border-r-2 border-r-lines">
+            <PageNav files={files} handleChangeEditor={handleChangeEditor} pageTitle={"professional-info"}/>
+        </nav>
+        <article className="w-full  border-t-lines lg:w-[41.4%] h-fit lg:h-full text-secondary text-xs ">
+            <LeftContent  openEditor={openEditor} handleChangeEditor={handleChangeEditor} closeEditor={closeEditor}/>
+        </article>
       <div className="lg:block hidden w-5 h-full mt-8 px-1 border-t-2 border-t-lines">
           <div className="w-3 h-full mx-auto mt-1 border-t-[6px] border-t-secondary">
           </div>
@@ -24,3 +56,26 @@ const ProfessionalInfo = () => {
 ProfessionalInfo.Layout = AboutLayout
 
 export default ProfessionalInfo
+
+
+export const getStaticProps= async()=>{
+  // const {files,error, loading, clearError, getData} = useAbout("professionalInfo")
+  const docsRef = collection(db,"professionalInfo")
+    const docs = await getDocs(docsRef)
+    const files = docs.docs.map(data=>data.data())
+    files.sort(dat=>{
+      if(dat.folderName==="none"){
+        return -1
+      }
+      else{
+        return 1
+      }
+    })
+    return {
+      props:{
+        files
+      }
+    }
+}
+
+
