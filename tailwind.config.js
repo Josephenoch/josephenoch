2bd5d0b4690b2e30f6f037e428212ba079288dfa
Joseph Enoch <enochjoseph01@gmail.com>
2024-06-07 01:10:16 +0100

    chore: fix tailwind issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./Components/**/*.{js,ts,jsx,tsx}",
-    "./Layouts/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
 
   ],
   theme: {
